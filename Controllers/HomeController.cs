6f570af453b26bf8b3755b45fa7a4eac4e13e5c6
drpo125 <drpo125@DRPO125-9>
2023-07-11 10:32:46 +0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuestApp.Models;
+using System.Diagnostics;
+
+
+namespace QuestApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly GuessingGameModel _gameModel;
+
+        public HomeController()
+        {
+            _gameModel = new GuessingGameModel();
+        }
+
+        public IActionResult Index()
+        {
+            return View(_gameModel);
+        }
+
+        [HttpPost]
+        public IActionResult Guess(GuessingGameModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _gameModel.UserGuess = model.UserGuess.ToLower().Trim();
+
+                // Проверяем, угадал ли пользователь загаданное слово
+                if (_gameModel.UserGuess == _gameModel.SecretWord)
+                {
+                    _gameModel.IsGameOver = true;
+                    _gameModel.Feedback = "Поздравляем! Вы угадали слово!";
+                }
+                else
+                {
+                    // Добавляем введенную версию в список предыдущих версий
+                    _gameModel.PreviousGuesses.Add(_gameModel.UserGuess);
+
+                    // Логика подсказок и ответов системы
+                    // ...
+
+                    // Пример ответа системы
+                    _gameModel.Feedback = "Нет, это не правильное слово. Попробуйте еще раз.";
+                }
+            }
+
+            return View("Index", _gameModel);
+        }
+    }
+}
---
+++ Models/GuessingGameModel.cs.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace QuestApp.Models
+{
+    public class GuessingGameModel
+    {
+        public string SecretWord { get; set; }
+        public List<string> PreviousGuesses { get; set; }
+        public string UserGuess { get; set; }
+        public string Feedback { get; set; }
+        public bool IsGameOver { get; set; }
+
+        public GuessingGameModel()
+        {
+            SecretWord = "banana"; // Загаданное слово (можно заменить на свое)
+            PreviousGuesses = new List<string>();
+            UserGuess = string.Empty;
+            Feedback = string.Empty;
+            IsGameOver = false;
+        }
+    }
+}
