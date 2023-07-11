cf9678a354ff97aea3ed74ef6f2d1a3967f47a4a
nochnoibriz@gmail.com <nochnoibriz@gmail.com>
2023-07-11 14:29:18 +0600

    llogic hint

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace QuestApp.Controllers
     public class HomeController : Controller
     {
         private readonly GuessingGameModel _gameModel;
-
+       
         public HomeController()
         {
             _gameModel = new GuessingGameModel();
@@ -27,7 +27,7 @@ namespace QuestApp.Controllers
                 _gameModel.UserGuess = model.UserGuess.ToLower().Trim();
 
                 // Проверяем, угадал ли пользователь загаданное слово
-                if (_gameModel.UserGuess == _gameModel.SecretWord)
+                if (_gameModel.UserGuess == _gameModel.SecretWord.ToLower().Trim())
                 {
                     _gameModel.IsGameOver = true;
                     _gameModel.Feedback = "Поздравляем! Вы угадали слово!";
@@ -35,17 +35,38 @@ namespace QuestApp.Controllers
                 else
                 {
                     // Добавляем введенную версию в список предыдущих версий
+
                     _gameModel.PreviousGuesses.Add(_gameModel.UserGuess);
 
                     // Логика подсказок и ответов системы
-                    // ...
+                    string hint = GenerateHint();
+                    if (_gameModel.UserGuess.Length > _gameModel.SecretWord.Length)
+                    {
+                        
+                    _gameModel.Feedback = "Загаданное слово короче";
+                    }
+                    if (_gameModel.UserGuess.Length < _gameModel.SecretWord.Length)
+                    {
+
+                        _gameModel.Feedback = "Загаданное слово длинее";
+                    }
+
 
                     // Пример ответа системы
-                    _gameModel.Feedback = "Нет, это не правильное слово. Попробуйте еще раз.";
+                    //_gameModel.Feedback = "Нет, это не правильное слово. Попробуйте еще раз.";
                 }
             }
 
             return View("Index", _gameModel);
+        }
+        private string GenerateHint()
+        {
+            // Здесь можно реализовать логику для генерации подсказки
+            // В данном примере просто возвращается случайная подсказка из списка
+            string[] hints = { "Загаданное слово длиннее", "Загаданное слово короче", "Подсказка: Первая буква " + _gameModel.SecretWord[0] };
+            Random random = new Random();
+            int index = random.Next(hints.Length);
+            return hints[index];
         }
     }
 }
--- Models/GuessingGameModel.cs.cs
+++ Models/GuessingGameModel.cs.cs
@@ -8,12 +8,14 @@ namespace QuestApp.Models
         public List<string> PreviousGuesses { get; set; }
         public string UserGuess { get; set; }
         public string Feedback { get; set; }
+        public int Attempt { get; set; }
         public bool IsGameOver { get; set; }
 
         public GuessingGameModel()
         {
             SecretWord = "banana"; // Загаданное слово (можно заменить на свое)
             PreviousGuesses = new List<string>();
+            Attempt = 0;
             UserGuess = string.Empty;
             Feedback = string.Empty;
             IsGameOver = false;
